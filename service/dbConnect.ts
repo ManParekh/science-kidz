5683c40c94db2c1797e99de0c32dbb85db20daaa
ManParekh <man.parekh@gmail.com>
2026-01-21 16:04:16 +0530

    Refactor dbConnect for improved connection handling
    Refactor dbConnect function to simplify connection checks and logging.

--- service/dbConnect.ts
+++ service/dbConnect.ts
@@ -1,22 +1,17 @@
 import mongoose from "mongoose";
 
-/**
- * Function to connect to the MongoDB database using Mongoose
- * Ensures a single connection instance is used throughout the application
- */
 export async function dbConnect() {
-  try {
-    if (mongoose.connections && mongoose.connections[0].readyState) {
-      console.log("Already connected to database");
-      return;
-    }
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
 
-    const { connection } = await mongoose.connect(process.env.ATLAS_DB_URL!, {
+  try {
+    await mongoose.connect(process.env.ATLAS_DB_URL as string, {
       dbName: "sciencekidz",
     });
-
-    console.log("Connected to database successfully", connection.host);
-  } catch (error: unknown) {
-    console.log("Database connection failed", error);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
   }
 }
